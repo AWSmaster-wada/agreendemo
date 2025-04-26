66041214f90ff27ebfd7d58b857682b116889056
AWSmaster-wada <yuto_yuto0928@icloud.com>
2025-04-26 23:49:31 +0900

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import JobListingGenerator from './components/JobListingGenerator';
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gradient-to-b from-green-50 to-amber-50">
+      <JobListingGenerator />
+    </div>
+  );
+}
+
+export default App;
